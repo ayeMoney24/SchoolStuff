8dcaea769c02869eab40db25ec9a3c93f8db7134
ayeMoney24 <32275218+ayeMoney24@users.noreply.github.com>
2017-11-27 16:41:55 -0500

    Work on HelloJq

---
+++ HelloJq/hello.js
@@ -0,0 +1,11 @@
+$("button#button").click(function(){
+	//DoStuff();
+	alert("Clicked");
+	console.log("hi");
+});
+
+
+//function DoStuff(){
+//	$(".p1").addClass("updated");
+//	console.log("HI");
+//}
