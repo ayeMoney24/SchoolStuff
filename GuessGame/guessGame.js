eff005b386e1328efbb90721ea4da1725c36b41c
ayeMoney24 <32275218+ayeMoney24@users.noreply.github.com>
2017-11-28 15:23:21 -0500

    guess game completed

---
+++ GuessGame/guessGame.js
@@ -0,0 +1,25 @@
+function myFunction() {
+	//var txt = document.getElementById("input");
+	var output = document.getElementById("output");
+	output.innerHTML = "Player 2 now tries to guess the number";
+	
+	
+}
+
+function guess(){
+	var num1 = parseInt(document.getElementById("input").value);
+	var num2 = parseInt(document.getElementById("input2").value);
+	var output = document.getElementById("output");
+	if(num2 > num1){
+		output.innerHTML = "Guess was too high";
+		document.getElementById("input2").value = ' ';
+	}
+	if(num2 < num1){
+		output.innerHTML = "Guess was too low";
+	}
+	if(num2 == num1){
+		output.innerHTML = "You one in only tries! Player 1 can select a new number";
+		document.getElementById("input").value = ' ';
+		document.getElementById("input2").value = null;
+	}
+}	
