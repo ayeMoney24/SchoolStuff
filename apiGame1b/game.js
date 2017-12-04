a6a462bcab3f5120add0c8cd4cdc891a9082a8b4
ayeMoney24 <32275218+ayeMoney24@users.noreply.github.com>
2017-12-04 14:35:48 -0500

    finished css

--- apiGame1b/game.js
+++ apiGame1b/game.js
@@ -21,6 +21,10 @@ function MyApp(){
 			OppPoints = increment(OppPoints);
 		});
 		
+		$("#restart").on("click" , function(){
+			restart();			
+		});
+		
 
 	};
 	
@@ -54,9 +58,8 @@ function MyApp(){
 	}
 	
 	function draw(DeckId){
-		console.log(DeckId);
 		$.ajax({
-			url: "https://deckofcardsapi.com/api/deck/fzg03owwpsth/draw/?count=1",
+			url: "https://deckofcardsapi.com/api/deck/7keenwohs9ia/draw/?count=1",
 			dataType: "json"
 		})
 			.done(function(data1) {cardsFunction(data1)})
@@ -76,12 +79,13 @@ function MyApp(){
 	function cardsFunction(data1){
 		$("#drawSpot").text("You drew a " + data1.cards[0].value + " of " + data1.cards[0].suit);
 		$("#PCard").attr("src",data1.cards[0].image);
-		
+		$("#PCard").removeClass("hidden");
 	}
 	
 	function cardsFunction2(data2){
 		$("#OppdrawSpot").text("CPU drew a " + data2.cards[0].value + " of " + data2.cards[0].suit);
 		$("#oppcard").attr("src",data2.cards[0].image);
+		$("#oppcard").removeClass("hidden");
 	}
 	
 	function WinHand(PPoints){
@@ -89,43 +93,34 @@ function MyApp(){
 		switch(PPoints){
 			case 0:
 				$("#playerCards").text("Player Points: 1");
-				PPoints = PPoints + 1;
 				break;
 			case 1:
 				$("#playerCards").text("Player Points: 2");
-				PPoints = 2;
 				break;
 			case 2:
 				$("#playerCards").text("Player Points: 3");
-				PPoints = 3;
 				break;
 			case 3:
 				$("#playerCards").text("Player Points: 4");
-				PPoints = 4;
 				break;
 			case 4:
 				$("#playerCards").text("Player Points: 5");
-				PPoints = 5;
 				break;
 			case 5:
 				$("#playerCards").text("Player Points: 6");
-				PPoints = 6;
 				break;
 			case 6:
 				$("#playerCards").text("Player Points: 7");
-				PPoints = 7;
 				break;
 			case 7:
 				$("#playerCards").text("Player Points: 8");
-				PPoints = 8;
 				break;
 			case 8:
 				$("#playerCards").text("Player Points: 9");
-				PPoints = 9;
 				break;
 			case 9:
 				$("#playerCards").text("10, you win the game!");
-				$("footer").append("Game over, refresh page to play again");
+				$("#restart").removeClass("hidden");
 				break;
 		}
 		
@@ -163,7 +158,7 @@ function MyApp(){
 				break;
 			case 9:
 				$("#OppCards").text("10, CPU wins the game!");
-				$("footer").append("Game over, refresh page to play again");
+				$("#restart").removeClass("hidden");
 				break;
 		}
 		
@@ -171,6 +166,10 @@ function MyApp(){
 	function increment(n){
 		n++;
 		return n;
+	}
+	
+	function restart(){
+		location.reload();
 	}
 
 }
